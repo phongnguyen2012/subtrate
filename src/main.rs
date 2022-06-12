5cb3fd76c7259734f9c73576fe7079644334c722
phongnguyen2012 <100146682+phongnguyen2012@users.noreply.github.com>
2022-06-12 14:15:22 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,12 @@ fn compare_sub_string(s1: &str, s2: String) -> (String, i32) {
     
     (s3, count)
 }
-
+fn input_string() -> String {
+    let mut input_string = String::new();
+    io::stdin().read_line(&mut input_string).expect("Failed to read line");
+    let input = input_string.trim();
+    input.to_string()
+}
 fn main() {
 
     let arr: [i32; 7] = [1, 2, 3, 4, 5, 6, 7];
@@ -38,20 +43,14 @@ fn main() {
     println!("Mang 2 la con mang 1 {}", ketqua);
     println!("*****************");
    
-
-    
-    let str1 = "mùa xuân, mùa hạ, mùa đông, mùa hè";
-    println!("Vui long nhap 1 từ");
-    let mut s4 = String::from("");
-    io::stdin().read_line(&mut s4).unwrap();
-    let s5 = s4.trim().to_string();
-    
-    let count_word = compare_sub_string(str1, s5.to_string());
-    println!("Word = {} and  count = {}", count_word.0, count_word.1);
-
-    
-
-   
+     let data = read_file("poem.txt");
+    let s = &*data;
+    println!("Moi ban nhap chuoi can tim");
+    let input = input_string();
+    println!("{}", s);
+    let s1 = compare_sub_string(s, input);
+    println!("Tu {} xuat hien {}", s1.0, s1.1);
+    println!("*****************");
     
 
 }
